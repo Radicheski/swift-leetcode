1366e4f03805732aedb26b3e21249ec9eb265d0f
Radicheski <erik.radicheski@gmail.com>
2022-08-15 14:21:46 -0300

    Solved "Richest Customer Wealth" problem

---
+++ LeetCode/RichestCustomerWealth.swift
@@ -0,0 +1,47 @@
+//
+//  RichestCustomerWealth.swift
+//  https://leetcode.com/problems/richest-customer-wealth/
+//
+//  Created by Erik Radicheski da Silva on 15/08/22.
+//
+
+import XCTest
+
+class RichestCustomerWealth: XCTestCase {
+
+    func test(){
+        let solution = Solution()
+
+        var accounts: [[Int]]
+
+        accounts = [[1, 2, 3],
+                    [3, 2, 1]]
+        XCTAssertEqual(solution.maximumWealth(accounts), 6)
+
+        accounts = [[1, 5],
+                    [7, 3],
+                    [3, 5]]
+        XCTAssertEqual(solution.maximumWealth(accounts), 10)
+
+        accounts = [[2, 8, 7],
+                    [7, 1, 3],
+                    [1, 9, 5]]
+        XCTAssertEqual(solution.maximumWealth(accounts), 17)
+    }
+
+}
+
+private class Solution {
+    func maximumWealth(_ accounts: [[Int]]) -> Int {
+        var result = [Int]()
+
+        for customer in 0..<accounts.count {
+            result.insert(0, at: customer)
+            for account in 0..<accounts[customer].count {
+                result[customer] += accounts[customer][account]
+            }
+        }
+
+        return result.max() ?? 0
+    }
+}
