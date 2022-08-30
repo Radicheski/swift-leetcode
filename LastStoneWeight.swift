814b9919aa653d6d5ff202917eb4cf2787204db7
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2022-08-30 09:24:45 -0300

    Solved "Day 15 - Heap" problems from "LeetCode 75"

---
+++ LastStoneWeight.swift
@@ -0,0 +1,40 @@
+//
+//  LastStoneWeight.swift
+//  https://leetcode.com/problems/last-stone-weight/
+//
+//  Created by Erik Radicheski da Silva on 30/08/22.
+//
+
+import XCTest
+
+class LastStoneWeight: XCTestCase {
+
+    func test() {
+        let solution = Solution()
+        
+        XCTAssertEqual(solution.lastStoneWeight([2, 7, 4, 1, 8, 1]), 1)
+        XCTAssertEqual(solution.lastStoneWeight([1]), 1)
+    }
+
+}
+
+private class Solution {
+    func lastStoneWeight(_ stones: [Int]) -> Int {
+        var stones = stones.sorted(by: >)
+        
+        while stones.count > 1 {
+            let stoneA = stones.removeFirst()
+            let stoneB = stones.removeFirst()
+            
+            if stoneA == stoneB {
+                continue
+            } else {
+                let newStone = abs(stoneA - stoneB)
+                stones.append(newStone)
+                stones.sort(by: >)
+            }
+        }
+        
+        return stones.isEmpty ? 0 : stones.removeFirst()
+    }
+}
---
+++ TopKFrequentWords.swift
@@ -0,0 +1,37 @@
+//
+//  TopKFrequentWords.swift
+//  https://leetcode.com/problems/top-k-frequent-words/
+//
+//  Created by Erik Radicheski da Silva on 30/08/22.
+//
+
+import XCTest
+
+class TopKFrequentWords: XCTestCase {
+
+    func test() {
+        let solution = Solution()
+        
+        XCTAssertEqual(solution.topKFrequent(["i","love","leetcode","i","love","coding"], 2), ["i","love"])
+        XCTAssertEqual(solution.topKFrequent(["the","day","is","sunny","the","the","the","sunny","is","is"], 4), ["the","is","sunny","day"])
+    }
+
+}
+
+private class Solution {
+    func topKFrequent(_ words: [String], _ k: Int) -> [String] {
+        let result = words.reduce(into: [:]) { result, word in
+            result[word, default: 0] += 1
+        }.sorted { (wordA: (key: String, value: Int), wordB: (key: String, value: Int)) in
+            if wordA.value == wordB.value {
+                return wordA.key < wordB.key
+            } else {
+                return wordA.value > wordB.value
+            }
+        }.reduce(into: []) {
+            $0.append($1.key)
+        }
+        
+        return Array(result[0 ..< k])
+    }
+}
