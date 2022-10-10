02bd68dc9ad0644195a6bf35daee5e773458b759
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2022-10-10 09:03:20 -0300

    Solved “Day 13 - Linked List” problems from “Data Structure II”

---
+++ LeetCode/ReorderList.swift
@@ -0,0 +1,59 @@
+//
+//  ReorderList.swift
+//  https://leetcode.com/problems/reorder-list/
+//
+//  Created by Erik Radicheski da Silva on 10/10/22.
+//
+
+import XCTest
+
+final class ReorderList: XCTestCase {
+    
+    private let solution = Solution()
+    
+    func testExampleA() {
+        let input = ListNode.createLinkedList([1, 2, 3, 4])
+        solution.reorderList(input)
+        let expectedOutput = ListNode.createLinkedList([1, 4, 2, 3])
+        XCTAssertEqual(expectedOutput, input)
+    }
+    
+    func testExampleB() {
+        let input = ListNode.createLinkedList([1, 2, 3, 4, 5])
+        solution.reorderList(input)
+        let expectedOutput = ListNode.createLinkedList([1, 5, 2, 4, 3])
+        XCTAssertEqual(expectedOutput, input)
+    }
+
+}
+
+private class Solution {
+    func reorderList(_ head: ListNode?) {
+        var list = [ListNode?]()
+        
+        var node = head
+        
+        while node != nil {
+            list.append(node)
+            node = node?.next
+        }
+        
+        var index = 1
+        var lastIndex = list.endIndex
+        
+        while index < lastIndex {
+            node = list.removeLast()
+            list.insert(node, at: index)
+            index += 2
+        }
+        
+        for (index, element) in list.enumerated() {
+            if index + 1 == list.count {
+                element?.next = nil
+                break
+            }
+            
+            element?.next = list[index + 1]
+        }
+    }
+}
---
+++ LeetCode/ReverseNodesInKGroup.swift
@@ -0,0 +1,66 @@
+//
+//  ReverseNodesInKGroup.swift
+//  https://leetcode.com/problems/reverse-nodes-in-k-group/
+//
+//  Created by Erik Radicheski da Silva on 10/10/22.
+//
+
+import XCTest
+
+final class ReverseNodesInKGroup: XCTestCase {
+    
+    private let solution = Solution()
+    
+    func testExampleA() {
+        let input = ListNode.createLinkedList([1, 2, 3, 4, 5])
+        let output = solution.reverseKGroup(input, 2)
+        let expectedOutput = ListNode.createLinkedList([2, 1, 4, 3, 5])
+        XCTAssertEqual(expectedOutput, output)
+    }
+    
+    func testExampleB() {
+        let input = ListNode.createLinkedList([1, 2, 3, 4, 5])
+        let output = solution.reverseKGroup(input, 3)
+        let expectedOutput = ListNode.createLinkedList([3, 2, 1, 4, 5])
+        XCTAssertEqual(expectedOutput, output)
+    }
+
+}
+
+private class Solution {
+    func reverseKGroup(_ head: ListNode?, _ k: Int) -> ListNode? {
+        guard head != nil else { return head }
+        
+        var node = head
+        
+        var list = [ListNode?]()
+        
+        while node != nil {
+            list.append(node)
+            node = node?.next
+        }
+        
+        var lastGroupIndex = list.count / k
+        
+        for index in 0 ..< lastGroupIndex {
+            var subrange = [ListNode?]()
+            
+            for _ in 0 ..< k {
+                subrange.append(list.remove(at: index * k))
+            }
+            
+            list.insert(contentsOf: subrange.reversed(), at: index * k)
+        }
+        
+        for (index, element) in list.enumerated() {
+            if index + 1 == list.count {
+                element?.next = nil
+                break
+            }
+            
+            element?.next = list[index + 1]
+        }
+        
+        return list.first ?? nil
+    }
+}
