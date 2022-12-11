33b4b406bfc98d3ab4aed2075e709c07895cf072
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2022-12-11 11:37:19 -0300

    Solved “Day 13” problems from “Dynamic Programming I”

---
+++ LeetCode/MinimumFallingPathSum.swift
@@ -0,0 +1,46 @@
+//
+//  MinimumFallingPathSum.swift
+//  https://leetcode.com/problems/minimum-falling-path-sum/
+//
+//  Created by Erik Radicheski da Silva on 11/12/22.
+//
+
+import XCTest
+
+final class MinimumFallingPathSum: XCTestCase {
+    
+    private let solution = Solution()
+    
+    func testExampleA() {
+        XCTAssertEqual(solution.minFallingPathSum([[2, 1, 3], [6, 5, 4], [7, 8, 9]]), 13)
+    }
+    
+    func testExampleB() {
+        XCTAssertEqual(solution.minFallingPathSum([[-19, 57], [-40, -5]]), -59)
+    }
+
+}
+
+private class Solution {
+    func minFallingPathSum(_ matrix: [[Int]]) -> Int {
+        guard matrix.count > 1 else { return matrix[0][0] }
+        
+        var result = matrix.last!
+        
+        for rowValue in matrix[..<(matrix.endIndex - 1)].reversed() {
+            var nextResult: [Int] = Array(repeating: .max, count: rowValue.count)
+            
+            for (columnIndex, columnValue) in rowValue.enumerated() {
+                var left = columnIndex == 0 ? Int.max : result[columnIndex - 1]
+                var center = result[columnIndex]
+                var right = columnIndex + 1 >= rowValue.endIndex ? Int.max : result[columnIndex + 1]
+                
+                nextResult[columnIndex] = min(left, center, right) + columnValue
+            }
+            
+            result = nextResult
+        }
+        
+        return result.min()!
+    }
+}
