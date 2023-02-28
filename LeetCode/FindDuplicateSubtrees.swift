c8ccd45ae9810d169b3cc1692c2ca89672c2ed30
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2023-02-28 08:43:28 -0300

    Solved daily challenge - 28/02/2023

---
+++ LeetCode/FindDuplicateSubtrees.swift
@@ -0,0 +1,49 @@
+//
+//  FindDuplicateSubtrees.swift
+//  https://leetcode.com/problems/find-duplicate-subtrees/
+//
+//  Created by Erik Radicheski da Silva on 28/02/23.
+//
+
+import XCTest
+
+final class FindDuplicateSubtrees: XCTestCase {
+
+    // MARK: Test cases
+
+}
+
+private class Solution {
+    func findDuplicateSubtrees(_ root: TreeNode?) -> [TreeNode?] {
+        var result = [TreeNode]()
+        
+        var tripletToID = [String: Int]()
+        var count = [Int: Int]()
+        
+        traverse(root, &tripletToID, &count, &result)
+        
+        return result
+    }
+    
+    private func traverse(_ node: TreeNode?,
+                          _ tripletToID: inout [String: Int],
+                          _ count: inout [Int: Int],
+                          _ result: inout [TreeNode]) -> Int {
+        guard let node = node else { return 0 }
+        
+        let leftId = traverse(node.left, &tripletToID, &count, &result)
+        let rightId = traverse(node.right, &tripletToID, &count, &result)
+        let triplet = "\(leftId),\(node.val),\(rightId)"
+        
+        let id = tripletToID[triplet] ?? tripletToID.count + 1
+        tripletToID[triplet] = id
+        
+        count[id, default: 0] += 1
+        
+        if count[id] == 2 {
+            result.append(node)
+        }
+        
+        return id
+    }
+}
